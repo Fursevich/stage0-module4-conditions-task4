a56aa4cbc008960a8e116414f25aeb848558cbc4
Furse <Fursevich.dasha@mail.ru>
2023-07-26 13:06:54 +0300

    ss

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        first = first <= second ? second : first;
+        first = first <= third ? third : first;
+        System.out.println(first);
     }
 }
